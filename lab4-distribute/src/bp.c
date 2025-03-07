6be3b22ce11e4cd4022708ca0e401a1475bc47c2
Charlie <carvajalchar25@uchicago.edu>
2025-03-06 21:32:06 -0600

    files from lab 3

--- lab4-distribute/src/bp.c
+++ lab4-distribute/src/bp.c
@@ -11,20 +11,59 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#define PHT_SIZE 256
+#define BTB_SIZE 1024
 
-void bp_predict(/*......*/)
+
+void bp_init(bp_t *bp)
 {
-    /* Predict next PC */
+    bp->ghr_bits = 8;
+    bp->ghr = 0;
+    bp->pht = calloc(PHT_SIZE, sizeof(uint8_t));
+
+    bp->btb_size = BTB_SIZE;
+    bp->btb_bits = 10;
+    bp->btb_tag = calloc(BTB_SIZE, sizeof(uint64_t));
+    bp->btb_dest = calloc(BTB_SIZE, sizeof(uint64_t));
+    bp->btb_valid = calloc(BTB_SIZE, sizeof(bool));
+    bp->btb_cond = calloc(BTB_SIZE, sizeof(bool));
 }
 
-void bp_update(/*......*/)
+void bp_predict(bp_t *bp, uint64_t *PC)
 {
-    /* Update BTB */
+    int btb_index = (*PC >> 2) & 0x3FF; 
+    if (bp->btb_valid[btb_index] && bp->btb_tag[btb_index] == *PC) {
+        if (!bp->btb_cond[btb_index]) {
+            *PC = bp->btb_dest[btb_index];
+        } 
+        else {
+            int pht_index = (bp->ghr ^ ((*PC >> 2) & 0xFF)); 
+            if (bp->pht[pht_index] >= 2) {
+                *PC = bp->btb_dest[btb_index];
+            }
+            else *PC += 4;
+        }
+    } 
+    else *PC += 4;
+}
 
+void bp_update(bp_t *bp, uint64_t PC, uint64_t target, bool taken, bool is_cond) 
+{    
+    if (is_cond) {
+        int pht_index = (bp->ghr ^ ((PC >> 2) & 0xFF));
 
-    /* Update gshare directional predictor */
+        if (taken && bp->pht[pht_index] < 3) bp->pht[pht_index]++;
+        else if (!taken && bp->pht[pht_index] > 0) bp->pht[pht_index]--;
+        bp->ghr = ((bp->ghr << 1) | (taken ? 1 : 0)) & 0xFF;
+    }
+    else if (!taken) target = PC + 4; 
 
-
-    /* Update global history register */
-
+    int btb_index = (PC >> 2) & 0x3FF;
+    if (!bp->btb_valid[btb_index] || bp->btb_tag[btb_index] != PC)
+    {
+        bp->btb_tag[btb_index] = PC;
+        bp->btb_dest[btb_index] = target;
+        bp->btb_valid[btb_index] = true;
+        bp->btb_cond[btb_index] = is_cond;
+    }
 }
--- lab4-distribute/src/bp.h
+++ lab4-distribute/src/bp.h
@@ -11,26 +11,27 @@
 #define _BP_H_
 
 #include <stdint.h>
+#include "stdbool.h"
 
 typedef struct
 {
-    /* gshare */
-    int ghr_bits;
-    uint32_t ghr;
-    uint8_t *pht; /* size 2^ghr_bits */
-
-    /* BTB */
-    int btb_size;
-    int btb_bits;
-    uint64_t *btb_tag;
-    uint64_t *btb_dest;
-    uint8_t *btb_valid;
-    uint8_t *btb_cond;
+     /* gshare */
+     int ghr_bits;
+     uint32_t ghr;
+     uint8_t *pht; 
+ 
+     /* BTB */
+     int btb_size;
+     int btb_bits;
+     uint64_t *btb_tag;
+     uint64_t *btb_dest;
+     uint8_t *btb_valid;
+     uint8_t *btb_cond;
 } bp_t;
 
 
-
-void bp_predict(/*......*/);
-void bp_update(/*......*/);
+void bp_init(bp_t *bp);
+void bp_predict(bp_t *bp, uint64_t *PC);
+void bp_update(bp_t *bp, uint64_t PC, uint64_t target, bool taken, bool is_cond);
 
 #endif
